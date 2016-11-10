72ca2f39a0aa79c32ec28a9eb65fd86201d4c83e
ChrisMagnuson <hourglasssand@gmail.com>
2016-11-10 13:53:45 -0500

    Initial commit

---
+++ BreakOnException.psm1
@@ -0,0 +1,8 @@
+﻿#Based on Roman Kuzmin's post http://stackoverflow.com/a/20913821/101679
+function Set-StackTraceBreakPoint {
+    $null = Set-PSBreakpoint -Variable StackTrace -Mode Write
+}
+
+function Remove-StackTraceBreakPoint  {
+    Get-PSBreakpoint -Variable StackTrace | Remove-PSBreakpoint
+}
